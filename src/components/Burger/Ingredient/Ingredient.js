6a8996d527cfe8ca47318259966d99ad77dcb304
vishaldeepak <vishal.deepak@outlook.com>
2017-12-18 11:34:50 +0530

    Added prop type for ingredients

--- src/components/Burger/Ingredient/Ingredient.js
+++ src/components/Burger/Ingredient/Ingredient.js
@@ -1,55 +1,62 @@
-import React from 'react'
+import React, {Component} from 'react'
 import classes from './Ingredient.css'
+import PropTypes from 'prop-types'
 
-const ingredient = (props) => {
-  let ingredientClass = null;
-  let ingredientContainer = null;
-  switch (props.key) {
-    case ('bread-top'):
-      ingredientClass = classes.BreadTop;
-      break;
-    case ('bread-bottom'):
-      ingredientClass = classes.BreadBottom;
-      break;
-    case ('seeds1'):
-      ingredientClass = classes.Seeds1;
-      break;
-    case ('seeds2'):
-      ingredientClass = classes.Seeds2;
-      break;
-    case ('meat'):
-      ingredientClass = classes.Meat;
-      break;
-    case ('cheese'):
-      ingredientClass = classes.Cheese;
-      break;
-    case ('salad'):
-      ingredientClass = classes.Salad;
-      break;
-    case ('bacon'):
-      ingredientClass = classes.Bacon;
-      break;
-    default:
-      ingredientClass = null
-      break;
-  }
+class Ingredient extends Component {
+  render() {
+    let ingredientClass = null;
+    let ingredientContainer = null;
+    switch (this.props.key) {
+      case ('bread-top'):
+        ingredientClass = classes.BreadTop;
+        break;
+      case ('bread-bottom'):
+        ingredientClass = classes.BreadBottom;
+        break;
+      case ('seeds1'):
+        ingredientClass = classes.Seeds1;
+        break;
+      case ('seeds2'):
+        ingredientClass = classes.Seeds2;
+        break;
+      case ('meat'):
+        ingredientClass = classes.Meat;
+        break;
+      case ('cheese'):
+        ingredientClass = classes.Cheese;
+        break;
+      case ('salad'):
+        ingredientClass = classes.Salad;
+        break;
+      case ('bacon'):
+        ingredientClass = classes.Bacon;
+        break;
+      default:
+        ingredientClass = null
+        break;
+    }
 
-  if(ingredientClass === classes.BreadTop)  {
-    ingredientContainer = (
-      <div className={classes.BreadTop}>
-        <div className={classes.Seeds1}></div>
-        <div className={classes.Seeds2}></div>
-      </div>
-    )
-  } else {
-    ingredientContainer = (
-      <div>
-        <div className={ingredientClass}></div>
-      </div>
-    )
+    if(ingredientClass === classes.BreadTop)  {
+      ingredientContainer = (
+        <div className={classes.BreadTop}>
+          <div className={classes.Seeds1}></div>
+          <div className={classes.Seeds2}></div>
+        </div>
+      )
+    } else {
+      ingredientContainer = (
+        <div>
+          <div className={ingredientClass}></div>
+        </div>
+      )
+    }
+    return ingredientContainer;
   }
-  return ingredientContainer;
 }
 
-export default ingredient
+Ingredient.propTypes = {
+  type: PropTypes.string.isRequired
+}
+
+export default Ingredient;
 
