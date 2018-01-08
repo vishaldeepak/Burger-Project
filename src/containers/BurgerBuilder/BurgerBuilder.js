a5b78506cd59adc194f4f422697adceddd1a3cce
vishaldeepak <vishal.deepak@outlook.com>
2018-01-08 12:23:06 +0530

    First approach to redux, setupt Burger Builder page for redux

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -7,6 +7,8 @@ import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
 import axios from '../../axios-orders'
 import Spinner from '../../components/UI/Spinner/Spinner'
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
+import {connect} from 'react-redux'
+import * as actionTypes from '../../store/actions'
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -17,8 +19,8 @@ const INGREDIENT_PRICES = {
 
 class BurgerBuilder extends Component{
   state = {
-    ingredients: null,
-    totalPrice: 4.0,
+    // ingredients: null,
+    // totalPrice: 4.0,
     purchasable: false,
     purchasing: false,
     loading: false,
@@ -28,7 +30,9 @@ class BurgerBuilder extends Component{
   componentDidMount(){
     axios.get("/ingredients.json")
       .then(response => {
-        this.setState({ingredients: response.data})
+        // this.setState({ingredients: response.data})
+        this.props.setIngredients({ingredients: response.data, totalPrice: 4.0})
+        this.updatePurchaseState(response.data)
       })
       .catch(error => {
         this.setState({error: true})
@@ -36,27 +40,29 @@ class BurgerBuilder extends Component{
   }
 
   addIngredientHandler = (type) => {
-      const updatedIngredients = {...this.state.ingredients};
-      updatedIngredients[type] = this.state.ingredients[type] + 1;
-      const newPrice = this.state.totalPrice + INGREDIENT_PRICES[type];
-      this.setState({
-        ingredients: updatedIngredients,
-        totalPrice: newPrice
-      })
+      const updatedIngredients = {...this.props.ingredients};
+      updatedIngredients[type] = this.props.ingredients[type] + 1;
+      const newPrice = this.props.totalPrice + INGREDIENT_PRICES[type];
+      this.props.setIngredients({ingredients: updatedIngredients, totalPrice: newPrice})
+      // this.setState({
+      //   ingredients: updatedIngredients,
+      //   totalPrice: newPrice
+      // })
       this.updatePurchaseState(updatedIngredients);
   }
 
   removeIngredientHandler = (type) => {
-    if(this.state.ingredients[type] <= 0){
+    if(this.props.ingredients[type] <= 0){
       return;
     }
-    const updatedIngredients = {...this.state.ingredients};
-    updatedIngredients[type] = this.state.ingredients[type] - 1;
-    const newPrice = this.state.totalPrice - INGREDIENT_PRICES[type];
-    this.setState({
-      ingredients: updatedIngredients,
-      totalPrice: newPrice
-    })
+    const updatedIngredients = {...this.props.ingredients};
+    updatedIngredients[type] = this.props.ingredients[type] - 1;
+    const newPrice = this.props.totalPrice - INGREDIENT_PRICES[type];
+    this.props.setIngredients({ingredients: updatedIngredients, totalPrice: newPrice})
+    // this.setState({
+    //   ingredients: updatedIngredients,
+    //   totalPrice: newPrice
+    // })
     this.updatePurchaseState(updatedIngredients);
   }
 
@@ -77,10 +83,10 @@ class BurgerBuilder extends Component{
 
   purchaseContinueHandler = () => {
     const queryParams=[]
-    for (let i in this.state.ingredients){
-      queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
+    for (let i in this.props.ingredients){
+      queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.props.ingredients[i]))
     }
-    queryParams.push("total_price="+this.state.totalPrice)
+    queryParams.push("total_price="+this.props.totalPrice)
     this.props.history.push({
       pathname: "/checkout",
       search: '?' + queryParams.join('&')
@@ -88,32 +94,32 @@ class BurgerBuilder extends Component{
   }
 
   render(){
-    const disableIngredients = {...this.state.ingredients};
+    const disableIngredients = {...this.props.ingredients};
     for( var key in disableIngredients){
       disableIngredients[key] = disableIngredients[key] <= 0;
     }
     let orderSummary = null
     let burger = this.state.error ? <p>Error Fetching Data!</p> : <Spinner />
 
-    if(this.state.ingredients){
+    if(this.props.ingredients){
       burger = (
         <Aux>
-          <Burger ingredients={this.state.ingredients}/>
+          <Burger ingredients={this.props.ingredients}/>
           <BuildControls
             ingredientAdded={this.addIngredientHandler}
             ingredientRemoved={this.removeIngredientHandler}
             disableIngredients={disableIngredients}
-            price={this.state.totalPrice}
+            price={this.props.totalPrice}
             purchasable={this.state.purchasable}
             orderNowHandler={this.purchaseHandler}
           />
         </Aux>
       )
       orderSummary = <OrderSummary
-      totalPrice={this.state.totalPrice}
+      totalPrice={this.props.totalPrice}
       purchaseCancel={this.purchaseCancelHandler}
       purchaseContinue={this.purchaseContinueHandler}
-      ingredients={this.state.ingredients}/>
+      ingredients={this.props.ingredients}/>
     }
 
     if(this.state.loading){
@@ -131,4 +137,17 @@ class BurgerBuilder extends Component{
   }
 }
 
-export default withErrorHandler(BurgerBuilder, axios)
+const mapStateToProps = state => {
+  return {
+    ingredients: state.ingredients,
+    totalPrice: state.totalPrice
+  };
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+      setIngredients: (payload) => dispatch({type: actionTypes.SET_INGREDIENTS, payload: payload})
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(BurgerBuilder, axios))
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,10 +1,20 @@
+import * as actionTypes from './actions'
+
 const initialState = {
   ingredients: [],
-  total_price: 0
+  totalPrice: 0.0
 }
 
 const reducer = (state=initialState, action) => {
-  return state;
+  switch (action.type) {
+    case actionTypes.SET_INGREDIENTS:
+      return {
+        ingredients: action.payload.ingredients,
+        totalPrice: action.payload.totalPrice
+      }
+    default:
+      return state;
+  }
 }
 
 export default reducer;
